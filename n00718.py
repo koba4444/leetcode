b7b9fac820748f0c609e3ae92e87a7cc5935ccc2
kok4444 <koba4444@gmail.com>
2022-09-20 12:06:22 +0300

    718

---
+++ n00718.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import collections
+
+
+class Solution:
+    def findLength(self, nums1, nums2) -> int:
+
+        pass
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+    sol = Solution()
+
+    print(sol.findLength([1,2,3,2,1], [3,2,1,4,7]))
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
