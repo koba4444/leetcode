5b3eca2f363f7d686f231135b2a9e1f92bb0d2c5
kok4444 <koba4444@gmail.com>
2022-10-17 19:52:00 +0300

    1832

---
+++ n01832.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import collections
+
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        aphbt = "abcdefghijklmnopqrstuvwxyz"
+        hash = {s:False for s in aphbt}
+        n = len(aphbt)
+        k = 0
+        for s in sentence:
+            if not hash[s]:
+                k += 1
+                hash[s] = True
+                if k == n: return True
+        return False
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+    sol = Solution()
+    print(sol.checkIfPangram("thequickbrownfoxjumpsoverthelazydog"))
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
