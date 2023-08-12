160d6b56ec5a68a2ab5539c19bc92795f3da5e5a
kok4444 <koba4444@gmail.com>
2023-08-13 00:40:39 +0300

    58

---
+++ n00058.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        l = len(s)
+        ans = 0
+        i = 0
+        while i < l:
+            if s[-1-i] == " ":
+                return ans
+            else:
+                ans += 1
+            i += 1
+        return ans
+
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+    sol = Solution()
+    print(sol.lengthOfLastWord("hello ll " ))
+
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
