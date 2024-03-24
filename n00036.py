aa01f820627e2df791a9fab63d5e0dbb5bdd7653
kok4444 <koba4444@gmail.com>
2024-03-24 17:05:13 +0300

    36

---
+++ n00036.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import collections
+
+
+
+class Solution:
+    def isValidSudoku(self, board) -> bool:
+
+
+
+if __name__ == "__main__":
+    start_time = datetime.now()
+    sol = Solution()
+    board =
+    [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+        , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+        , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+        , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+        , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+        , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+        , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+        , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+        , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+    print(sol.isValidSudoku(board))
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
